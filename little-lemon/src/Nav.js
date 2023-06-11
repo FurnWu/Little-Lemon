a3fef6e9b8877bfba14b724080596b86f3d297dd
FurnWu <125434335+FurnWu@users.noreply.github.com>
2023-06-11 20:49:02 +0700

    Add components

--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -1,10 +1,17 @@
-import myLogo from './Assets/Logo.svg'
+import logo from './Assets/Logo.svg'
 function Nav() {
     return (
     <nav>
-        <img src={myLogo} alt="Little Lemon Logo" />
-    </nav>
 
+            <img src={logo} alt="Little Lemon Logo" />
+            <ul>Home</ul>
+            <ul>About</ul>
+            <ul>Menu</ul>
+            <ul>Reservations</ul>
+            <ul>Order Online</ul>
+            <ul>Login</ul>
+
+    </nav>
     );
 };
 
