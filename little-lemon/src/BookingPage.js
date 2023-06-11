530e4cd6d53174a428b8cd08d8fd812f85dc4a33
FurnWu <125434335+FurnWu@users.noreply.github.com>
2023-06-12 00:13:11 +0700

    Add routes

---
+++ little-lemon/src/BookingPage.js
@@ -0,0 +1,7 @@
+function Booking() {
+    return(
+        <h1>Reservations</h1>
+    );
+    };
+    
+    export default Booking;
--- little-lemon/src/Nav.js
+++ little-lemon/src/Nav.js
@@ -1,17 +1,28 @@
 import logo from './Assets/Logo.svg'
+import {Routes, Route, Link} from 'react-router-dom';
+import Home from './Homepage';
+import Booking from './BookingPage';
 function Nav() {
     return (
-    <nav>
+    <div className='nav-main'>
+         <nav>
 
             <img src={logo} alt="Little Lemon Logo" />
-            <ul>Home</ul>
+            <ul><Link to="/" className="nav-item">Home</Link></ul>
             <ul>About</ul>
             <ul>Menu</ul>
-            <ul>Reservations</ul>
+            <ul><Link to="/reservations" className="nav-item">Reservations</Link></ul>
             <ul>Order Online</ul>
             <ul>Login</ul>
 
     </nav>
+    <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/reservations" element={<Booking />} />
+    </Routes>
+
+    </div>
+
     );
 };
 
