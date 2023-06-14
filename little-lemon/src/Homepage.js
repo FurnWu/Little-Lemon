b696d5e71df7f39b68b99a4369c235421b5a699b
FurnWu <125434335+FurnWu@users.noreply.github.com>
2023-06-14 14:02:45 +0700

    Add hero content

--- little-lemon/src/Homepage.js
+++ little-lemon/src/Homepage.js
@@ -1,18 +1,29 @@
 import heroImage from './Assets/restauranfood.jpg';
+import Booking from './BookingPage';
+import {Routes, Route, Link} from 'react-router-dom';
 function Home() {
 return(
-    <div className="Home">
+    <div className="home">
 
         <div className="hero">
-            <header style={{backgroundColor: 'gray'}}>
+            <header style={{backgroundColor: 'grey'}}>
                 <h1 style={{color: '#F4CE14'}}>Little Lemon</h1>
                 <h3 style={{color: 'white'}}>Chicago</h3>
                 <p style={{color: 'white'}}>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
             </header>
-            <section>
-                <img src={heroImage} alt="Restaurant Food" />
-            </section>
+            <div className="btn">
+                <Link to="/reservations" className="btn" style={{ textDecoration: 'none' }}>
+                    <button>Reserve a table</button>
+                </Link>
+            </div>
+            
+        <Routes>
+            <Route path="/reservations" element={<Booking />} />
+        </Routes>
         </div>
+        <section>
+                <img src={heroImage} alt="Restaurant Food" className='foodhero' />
+        </section>
     </div>
 
 );
