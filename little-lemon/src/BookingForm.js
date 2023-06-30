b7d1be12d566c6fdb06488981a500c68f7949046
FurnWu <125434335+FurnWu@users.noreply.github.com>
2023-06-30 16:22:00 +0700

    Added state

--- little-lemon/src/BookingForm.js
+++ little-lemon/src/BookingForm.js
@@ -1,17 +1,30 @@
 import { useState } from "react";
 
-function BookingForm() {
-    
+ function BookingForm() {
+    const[date, setDate] = useState("")
+    const[availableTimes, setAvailableTimes] = useState("")
+    const[guests, setGuests] = useState("1")
+    const[occasion, setOccasion] = useState("")
+
+    const handleSubmit = (e) => {
+      e.preventDefault();}
+
     return(
+
 <form style={{display: "grid; max-width: 200px; gap: 20px"}}>
+
 <div className= "date">
     <label htmlForfor="res-date">Choose date</label>
-   <input type="date" id="res-date" />
+   <input
+         type="date"
+         id="res-date"
+         value={date}
+         onChange={(e) => setDate(e.target.value)}/>
 </div>
-   
+
 <div className="time">
     <label htmlFor="res-time">Choose time</label>
-   <select id="res-time ">
+   <select id="res-time " value ={availableTimes} onChange={(e) => setAvailableTimes(e.target.value)}>
       <option>17:00</option>
       <option>18:00</option>
       <option>19:00</option>
@@ -20,28 +33,31 @@ function BookingForm() {
       <option>22:00</option>
    </select>
 </div>
-   
-<div className="guest">
+
+<div className="guest" >
     <label htmlFor="guests">Number of guests</label>
-    <input type="number" placeholder="1" min="1" max="10" id="guests" />
+    <input type="number" placeholder="1" min="1" max="10" id="guests"
+           value = {guests} onChange={(e) => setGuests(e.target.value)}   />
+
 </div>
+
    <div className="occasion">
-    <label htmlFor="occasion">Occasion</label>
-   <select id="occasion">
+    <label htmlFor ="occasion">Occasion</label>
+   <select id ="occasion" value={occasion} onChange={(e) => setOccasion(e.target.value)}>
       <option>Birthday</option>
       <option>Anniversary</option>
+      <option>Engagement</option>
+      <option>Just chill and eat</option>
       <option>Other</option>
    </select>
    </div>
    
-   <input type="submit" value="Make Your reservation" />
+   <input type="submit" value="Make Your reservation" onSubmit={handleSubmit} />
 </form>
      
       
 
     );
     };
-
-
 
     export default BookingForm;
