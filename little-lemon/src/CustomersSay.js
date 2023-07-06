bbee96bb75bd5390f91f754ee52c7cee0c78e512
FurnWu <125434335+FurnWu@users.noreply.github.com>
2023-07-06 21:11:20 +0700

    Testimonials-just started

---
+++ little-lemon/src/CustomersSay.js
@@ -0,0 +1,17 @@
+import './Testimonial.css';
+
+const comments = [
+    {
+        customerName: "Nora H.",
+        comment: ""
+    },
+    {
+        customerName: "Alex D.",
+    },
+    {
+        customerName: "Henry W.",
+    },
+    {
+        customerName: "Gabriella S.",
+    }
+]
