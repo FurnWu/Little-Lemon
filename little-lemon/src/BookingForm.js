159d7bb37526c9b23b0d5ff6d2067cf38794dc9f
FurnWu <125434335+FurnWu@users.noreply.github.com>
2023-07-12 01:23:41 +0700

    booking form almost done

--- little-lemon/src/BookingForm.js
+++ little-lemon/src/BookingForm.js
@@ -1,36 +1,54 @@
-import { useState } from "react";
+import { useState, useRef} from "react";
 import { validateEmail } from "./utils";
 import { fetchAPI } from "./DataAPI";
-
+import {VscWarning} from "react-icons/vsc";
  function BookingForm() {
+   
+   const hen = useRef(null);
    const [isDisabled, setIsDisabled] = useState(false);
    const [formData, setFormData] = useState({firstName: "", lastName: "",email: "",phone: "",message: ""});
    const [inputError, setInputError] = useState(null);
-  const handleChange = (event) => {
+   const [inputPhoneError, setInputPhoneError] = useState(null);
+
+   const handleChange = (event) => {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
-
+      
     if (value.length < 2) {
       setInputError('Input must be at least 2 characters');
     } else {
       setInputError(null);
     }
   };
+
+  const handleValidPhoneNumber = (event) => {
+   const { name, value } = event.target;
+   setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+
+   if (value.length !== 10) {
+     setInputPhoneError('Phone number must be 10 digits');
+   }
+   else {
+     setInputPhoneError(null);
+   }
+ };
+
   const getIsFormValid = () => {
    return (
      formData.firstName &&
      formData.lastName &&
      formData.phone &&
      validateEmail(formData.email) &&
-     occasion !== "F"
+     occasion !== "F" &&
+     chosenTime !== "T"
    );
  };
 var curr = new Date();
 curr.setDate(curr.getDate());
-var dfaultDate = curr.toISOString().substring(0,10);
+//var dfaultDate = curr.toISOString().substring(0,10);
 
-    const[date, setDate] = useState(dfaultDate)
-    const[chosenTime, setChosenTime] = useState("")
+    const[date, setDate] = useState("");
+    const[chosenTime, setChosenTime] = useState("T")
     let defaultTime = fetchAPI(date)
     const listDefaultTime = defaultTime.map(time => <option>{time}</option>)
     const[guests, setGuests] = useState("1")
@@ -49,12 +67,15 @@ var dfaultDate = curr.toISOString().substring(0,10);
    }
    //console.log(fetchAPI(date));
     const handleSubmit = (e) => {
-      setIsDisabled(true)
       e.preventDefault();
-      alert("Reservation Completed!");
+      setIsDisabled(true);
+      alert("Make your reservation now!");
+      
+      //myFunction();
       //clearForm();
    }
-console.log(formData);
+   console.log(isDisabled);
+   console.log(formData);
 
    /*const handleSubmitClick = (e) => {
       setIsDisabled(true)
@@ -62,7 +83,7 @@ console.log(formData);
    }*/
 
    const handleEditClick = (e) => {
-      setIsDisabled(false)
+      setIsDisabled(false);
       
       e.preventDefault();
    }
@@ -82,18 +103,19 @@ console.log(occasion);
             value={formData.firstName} 
             onChange={handleChange}
             required />
-            {inputError && <div style={{ color: '#F4CE14', fontSize: '12px' }}>{inputError}</div>}
+            {inputError && <div style={{ color: '#F4CE14', fontSize: '12px' }}><VscWarning/> {inputError}</div>}
    </div>
    <div className="lastname"><label htmlFor="lasttName">Last Name<sup>*</sup></label><br></br>
    <input type="text" id="lastName" name="lastName" disabled={isDisabled} placeholder="Your last name ..." value={formData.lastName} onChange={handleChange} required/>
-      {inputError && <div style={{ color: '#F4CE14', fontSize: '12px' }}>{inputError}</div>}
+      {inputError && <div style={{ color: '#F4CE14', fontSize: '12px' }}><VscWarning/> {inputError}</div>}
    </div>
 </div>
 <br></br>
 <div className="contactInfo">
    <div className="phonenumber"><label htmlFor="phone">Phone Number<sup>*</sup></label><br></br>
-      <input type="tel" id="phone" name="phone" disabled={isDisabled} pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="xxx-xxx-xxxx"  value={formData.phone} onChange={handleChange} required/></div>
-
+      <input type="tel" id="phone" name="phone" disabled={isDisabled} pattern="[0-9]{10}" placeholder="10 digits phone number "  value={formData.phone} onChange={handleValidPhoneNumber} required/>
+      {inputPhoneError && <div style={{ color: '#F4CE14', fontSize: '12px' }}><VscWarning/> {inputPhoneError}</div>}
+      </div>
    <div className="email"><label htmlFor="email">Email<sup>*</sup></label><br></br>
       <input type="email" id="email" name="email" disabled={isDisabled} value={formData.email} placeholder="example@mail.com" onChange={handleChange} required/></div>
 </div>
@@ -112,7 +134,9 @@ console.log(occasion);
 <div className="time">
     <label htmlFor="res-time">Time<sup>*</sup></label><br></br>
    <select id="res-time " disabled={isDisabled} value={chosenTime} onChange={(e) => setChosenTime(e.target.value)}>
-      {listDefaultTime}
+      <optgroup label="Pick available time">
+         <option hidden="true" value="T">Pick time</option>
+         {listDefaultTime}</optgroup>
    </select>
 </div>
 
@@ -123,7 +147,7 @@ console.log(occasion);
    <div className="occasion">
     <label htmlFor ="occasion">Occasion<sup>*</sup></label><br></br>
    <select id ="occasion" disabled={isDisabled} value={occasion} onChange={(e) => setOccasion(e.target.value)}>
-   <optgroup label="Choose you occasion">
+   <optgroup label="Choose your occasion">
       <option hidden="true" value="F">In what occasion?</option>
       <option value="Eat and Chill">Eat and Chill</option>
       <option value="Friends/Family Reuion">Friends/Family Reuion</option>
@@ -154,35 +178,49 @@ console.log(occasion);
    {/*<button onClick={handleSubmitClick}>Submit</button>*/}
    <div className="form-button">
       
-      <button className="booking-button" type="submit" onSubmit={handleSubmit} disabled={!getIsFormValid()} >Book Now!</button>
+      <button onClick={() => hen.current.scrollIntoView({ behavior: "smooth", block: "end" })} className="booking-button" type="submit" onSubmit={handleSubmit} disabled={!getIsFormValid()} >Book Now!</button>
       
    </div>
    
 </form>
 <br></br>
-<Confirmation/>
+<div ref={hen} >
+  <Confirmation/> 
+</div>
+
 </div>
  );
 
 function Confirmation() {
-   if (isDisabled)
-{return (
-   <div style={{backgroundColor:"#EDEFEE"}}>
-      <h2>Please recheck the information</h2>
-      <h3>Name: {formData.firstName}&nbsp;&nbsp;{formData.lastName}</h3>
-      <h3>Date and Time: {date}/&nbsp;{chosenTime}</h3>
-      <h3>Number of Guests: {guests}</h3>
-      <h3>Occasion: {occasion}</h3>
-      <h3>Additional Request: {formData.message}</h3>
-      <h3>Phone Number: {formData.phone}</h3>
-      <h3>Email: {formData.email}</h3>
-      <button className="edit-button" onClick={handleEditClick}>Edit</button>
+   if (isDisabled) {
+   return (
+   <div className="confirm" style={{backgroundColor:"white", textAlign:"left", paddingBlock:"2rem"}} >
+      <h1 style={{textAlign:"center", paddingBottom:"1rem", color:"#333333"}}>Confirm reservation</h1>
+      <div style={{backgroundColor:"white"}}>
+         <h3 style={{display:"inline-flex"}}><div style={{width:"15rem", color:"#495E57"}}>NAME :</div>{formData.firstName}&nbsp;&nbsp;{formData.lastName}</h3><br></br>
+         <h3 style={{display:"inline-flex"}}><div style={{width:"15rem", color:"#495E57"}}>DATE AND TIME :</div>{date}&nbsp;/&nbsp;&nbsp;{chosenTime}</h3><br></br>
+         <h3 style={{display:"inline-flex"}}><div style={{width:"15rem", color:"#495E57"}}>NUMBER OF GUESTS :</div>{guests}</h3><br></br>
+         <h3 style={{display:"inline-flex"}}><div style={{width:"15rem", color:"#495E57"}}>OCCASION :</div>{occasion}</h3><br></br>
+         <h3 style={{display:"inline-flex"}}><div style={{width:"15rem", color:"#495E57"}}>ADDITIONAL REQUEST :</div>{formData.message}</h3><br></br>
+         <h3 style={{display:"inline-flex"}}><div style={{width:"15rem", color:"#495E57"}}>PHONE NUMBER :</div>{formData.phone}</h3><br></br>
+         <h3 style={{display:"inline-flex"}}><div style={{width:"15rem", color:"#495E57"}}>E-MAIL :</div>{formData.email}</h3><br></br> 
+      </div>
+      <div className="res-button">
+         <button id="editbtn" className="edit-button" onClick={handleEditClick}>Edit</button>
+         <button className="confirm-button">Confirm</button>
+      </div>
+      
    </div>
 )
 }
 
-};
+}
 
+
+/*function myFunction() {
+   const element = document.getElementById("confirm");
+   element.scrollIntoView();
+}*/
     };
 
 
